348d2091c43fa8fadf52a43cd7e4e152937feb0b
Baron Tang <tangtang960225@163.com>
2024-04-12 16:33:32 +0800

    feat: 提升打包构建速度的方式-oneOf

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -21,66 +21,70 @@ module.exports = {
   // 加载器
   module: {
     rules: [
-      // loader配置
-      // use执行顺序： 从后向前
-      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
       {
-        test: /\.less$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // 将 JS 字符串生成为 style 节点
-          // "style-loader",
-          MiniCssExtractPlugin.loader,
-          // 将 CSS 转化成 CommonJS 模块
-          "css-loader",
-          // 将 Sass 编译成 CSS
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.styl$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|webp|svg)$/,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024,
+        oneOf: [
+          // loader配置
+          // use执行顺序： 从后向前
+          { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+          {
+            test: /\.less$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
           },
-        },
-        // 自定义文件名称，默认是hash值，如果不想hash值，可以设置为none
-        generator: {
-          filename: "static/images/[hash:10][ext][query]",
-        },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "static/fonts/[hash:10][ext][query]",
-        },
-      },
-      // 处理其他资源
-      {
-        test: /\.(mp3|mp4|avi)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "static/media/[hash:10][ext][query]",
-        },
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/, // 排除node_modules文件夹
-        use: {
-          loader: "babel-loader",
-          // options: {
-          //   presets: ["@babel/preset-env"],
-          // },
-        },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // 将 JS 字符串生成为 style 节点
+              // "style-loader",
+              MiniCssExtractPlugin.loader,
+              // 将 CSS 转化成 CommonJS 模块
+              "css-loader",
+              // 将 Sass 编译成 CSS
+              "sass-loader",
+            ],
+          },
+          {
+            test: /\.styl$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+          },
+          {
+            test: /\.(png|jpe?g|gif|webp|svg)$/,
+            type: "asset",
+            parser: {
+              dataUrlCondition: {
+                maxSize: 10 * 1024,
+              },
+            },
+            // 自定义文件名称，默认是hash值，如果不想hash值，可以设置为none
+            generator: {
+              filename: "static/images/[hash:10][ext][query]",
+            },
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: "asset/resource",
+            generator: {
+              filename: "static/fonts/[hash:10][ext][query]",
+            },
+          },
+          // 处理其他资源
+          {
+            test: /\.(mp3|mp4|avi)$/i,
+            type: "asset/resource",
+            generator: {
+              filename: "static/media/[hash:10][ext][query]",
+            },
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/, // 排除node_modules文件夹
+            use: {
+              loader: "babel-loader",
+              // options: {
+              //   presets: ["@babel/preset-env"],
+              // },
+            },
+          },
+        ],
       },
     ],
   },
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -24,7 +24,7 @@ function getStyleLoader(pre) {
 module.exports = {
   // 模式
   mode: "production", // 开发模式
-  devtool: "cheap-module-source-map"
+  devtool: "cheap-module-source-map",
   //   mode: "production", // 生产模式
   // 入口
   entry: "./src/main.js", // 相对路径
@@ -39,61 +39,65 @@ module.exports = {
   // 加载器
   module: {
     rules: [
-      // loader配置
-      // use执行顺序： 从后向前
       {
-        test: /\.css$/,
-        use: getStyleLoader(),
-      },
-      {
-        test: /\.less$/i,
-        use: getStyleLoader("less-loader"),
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: getStyleLoader("sass-loader"),
-      },
-      {
-        test: /\.styl$/,
-        use: getStyleLoader("stylus-loader"),
-      },
-      {
-        test: /\.(png|jpe?g|gif|webp|svg)$/,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024,
+        oneOf: [
+          // loader配置
+          // use执行顺序： 从后向前
+          {
+            test: /\.css$/,
+            use: getStyleLoader(),
           },
-        },
-        // 自定义文件名称，默认是hash值，如果不想hash值，可以设置为none
-        generator: {
-          filename: "static/images/[hash:10][ext][query]",
-        },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "static/fonts/[hash:10][ext][query]",
-        },
-      },
-      // 处理其他资源
-      {
-        test: /\.(mp3|mp4|avi)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "static/media/[hash:10][ext][query]",
-        },
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/, // 排除node_modules文件夹
-        use: {
-          loader: "babel-loader",
-          // options: {
-          //   presets: ["@babel/preset-env"],
-          // },
-        },
+          {
+            test: /\.less$/i,
+            use: getStyleLoader("less-loader"),
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: getStyleLoader("sass-loader"),
+          },
+          {
+            test: /\.styl$/,
+            use: getStyleLoader("stylus-loader"),
+          },
+          {
+            test: /\.(png|jpe?g|gif|webp|svg)$/,
+            type: "asset",
+            parser: {
+              dataUrlCondition: {
+                maxSize: 10 * 1024,
+              },
+            },
+            // 自定义文件名称，默认是hash值，如果不想hash值，可以设置为none
+            generator: {
+              filename: "static/images/[hash:10][ext][query]",
+            },
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: "asset/resource",
+            generator: {
+              filename: "static/fonts/[hash:10][ext][query]",
+            },
+          },
+          // 处理其他资源
+          {
+            test: /\.(mp3|mp4|avi)$/i,
+            type: "asset/resource",
+            generator: {
+              filename: "static/media/[hash:10][ext][query]",
+            },
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/, // 排除node_modules文件夹
+            use: {
+              loader: "babel-loader",
+              // options: {
+              //   presets: ["@babel/preset-env"],
+              // },
+            },
+          },
+        ],
       },
     ],
   },
@@ -109,7 +113,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "static/css/main.css",
     }),
-    new CssMinimizerPlugin()
+    new CssMinimizerPlugin(),
   ],
   devServer: {
     host: "localhost",
